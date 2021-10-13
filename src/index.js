bfe741008f5dbbf4159026806408f0733f2cc2df
Joshua Signey <Joshua.Signey@antser.com>
2021-10-14 02:40:23 +0800

    Added react modals and removed reference to app.css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
 
 ReactDOM.render(
   <React.StrictMode>
